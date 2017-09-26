3fa52e8ba128a7778eb5fd992a9ac094cb27e91f
gastrodia <gastrodia@outlook.com>
2017-09-26 17:34:07 +0800

    layout

--- src/components/ZZAdminLayout.js
+++ src/components/ZZAdminLayout.js
@@ -1,11 +1,31 @@
 import Head from 'preact-head'
-import {h} from 'preact'
+import { h } from 'preact'
 import Layout from './Layout'
-
-export default ({ menu,children}) => (
+import '../styles/zzadmin.css'
+import SidebarMenu from './SidebarMenu';
+export default ({ logo,user, menu, children }) => (
     <Layout>
-        {menu}
+        <div class="zzadmin-header">
+            <div class="logo">
+                <img src={logo.img} alt={logo.alt} />
+                <span>{logo.text}</span>
+            </div>
+
+            <div class="user">
+                <img src={user.img} alt={logo.alt}/>
+                <span class="username">{logo.username}</span>
+                <button class="edit-passwd">修改密码</button>
+                <button class="exit">退出</button>
+            </div>
+        </div>
+
+        <div class="zzadmin-body">
+            <div class="sidebar">
+                <SidebarMenu data={[menu]} />
+            </div>
+        </div>
 
         {children}
     </Layout>
 )
+
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 
 export default {
-<<<<<<< HEAD
 	Button: require('./Button/Button').default,
 	Icon: require('./Icon/Icon').default,
 	Modals: require('./Modals/Modals').default,
@@ -10,12 +9,5 @@ export default {
 	Form: require('./Form/Form').default,
 	FormItem: require('./Form/FormItem').default,
 	Sidebar: require('./Sidebar/Sidebar').default
-=======
-    Button:require('./Button/Button').default,
-    Icon:require('./Icon/Icon').default,
-    Modals:require('./Modals/Modals').default,
-    Sliders:require('./Sliders/Sliders').default,
-    Timeline:require('./Timeline/Timeline').default,
-    Layout:require('./Layout').default
->>>>>>> c04b8feb01148e4dc3700e9e396e3d6aa92334e8
 }
+ 
