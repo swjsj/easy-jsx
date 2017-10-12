f0b1f2713fbe6030dae70776baaa4db4be679352
baichanc <314973566@qq.com>
2017-10-12 14:12:45 +0800

    更改zzstyle

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -4,13 +4,13 @@ import * as util from '../util'
 export default class Sidebar extends Component {
 
     getTreeView() {
+        
         var menu = this.props.data.menu;
         var map = this.map = util.list2map(menu)
         var menuTree = util.list2tree(menu);
         var viewList = menuTree.map((item) => { return this.getTreeItemView(item) })
         return viewList;
     }
-
 
   
 
