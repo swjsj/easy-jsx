8edaee523d684ae94e1acb2025933b7572130777
baichanc <314973566@qq.com>
2017-10-18 17:58:07 +0800

    widgets

---
+++ src/components/widgets/Messaging.js
@@ -0,0 +1,16 @@
+import { Component, h } from 'preact'
+
+import './widgets.less'
+
+export default class Messaging extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+
+    }
+    
+}
